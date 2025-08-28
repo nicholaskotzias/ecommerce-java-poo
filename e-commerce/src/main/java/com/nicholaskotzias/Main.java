a04968fb4d607913d9c961568aaba260875d41ff
nicholaskotzias <nicokotzias@gmail.com>
2025-08-28 12:09:11 -0300

    feat: adiciona ArrayList de produtos e configuração inicial do Javalin na Main

--- e-commerce/src/main/java/com/nicholaskotzias/Main.java
+++ e-commerce/src/main/java/com/nicholaskotzias/Main.java
@@ -1,8 +1,13 @@
 package com.nicholaskotzias;
 
+import com.nicholaskotzias.models.Produto;
 import io.javalin.Javalin;
+import java.util.ArrayList;
+
+
 
 public class Main {
+    public static ArrayList<Produto> bdProdutos;
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
