a22b88774319424e9a5a36326cd333187ed55f79
nicholaskotzias <nicokotzias@gmail.com>
2025-08-28 11:40:57 -0300

    feat: adicionar classe Produto com atributos nome, preço e estoque

---
+++ e-commerce/src/main/java/com/nicholaskotzias/models/Produto.java
@@ -0,0 +1,37 @@
+package com.nicholaskotzias.models;
+
+public class Produto {
+    private String nome;
+    private Double preco;
+    private Integer estoque;
+
+    public Produto(String nome, Double preco, Integer estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    public Integer getEstoque() {
+        return estoque;
+    }
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome + " - " + this.preco;
+    }
+}
