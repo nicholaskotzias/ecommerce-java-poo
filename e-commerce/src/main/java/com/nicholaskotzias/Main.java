25b8d6fa1e813ca5b3c16b896e1a49a3a87ab649
nicholaskotzias <nicokotzias@gmail.com>
2025-08-28 11:09:52 -0300

    feat: adiciona servidor básico com Javalin para rota de teste ao Main.java

---
+++ e-commerce/src/main/java/com/nicholaskotzias/Main.java
@@ -0,0 +1,13 @@
+package com.nicholaskotzias;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Javalin app = Javalin.create().start(7070);
+
+        app.get("/tads24", ctx -> ctx.result("TADS24 EH TOP"));
+    }
+}
