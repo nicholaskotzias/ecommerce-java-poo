a38fc4b6c1623063715d4c1d05e9a777e6c484fa
nicholaskotzias <nicokotzias@gmail.com>
2025-09-04 10:59:25 -0300

    feat: adicionar a classe Carrinho com atributos e métodos para gerenciar produtos em um carrinho de compras

---
+++ e-commerce/src/main/java/com/nicholaskotzias/models/Carrinho.java
@@ -0,0 +1,72 @@
+package com.nicholaskotzias.models;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
+
+public class Carrinho {
+    private ArrayList<Produto> produtos;
+    private Double total;
+    private String descricao;
+    private LocalDateTime criadoEm;
+    private LocalDateTime finalizadoEm;
+
+    public Carrinho(Double total, String descricao, LocalDateTime criadoEm,
+            LocalDateTime finalizadoEm) {
+        this.produtos = new ArrayList<>();
+        this.total = total;
+        this.descricao = descricao;
+        this.criadoEm = criadoEm;
+        this.finalizadoEm = finalizadoEm;
+    }
+
+    public Carrinho(LocalDateTime criadoEm){
+        this.produtos = new ArrayList<>();
+        this.total = 0.0;
+        this.descricao = "PENDENTE";
+        this.criadoEm = criadoEm;
+        this.finalizadoEm = null;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getFinalizadoEm() {
+        return finalizadoEm;
+    }
+
+    public void setFinalizadoEm(LocalDateTime finalizadoEm) {
+        this.finalizadoEm = finalizadoEm;
+    }
+    
+    
+}  
